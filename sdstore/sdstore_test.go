ff1c790dee375a4b74e1d42266eb4492814f9764
Dao Lam <daolam112@gmail.com>
2018-09-20 15:06:30 -0700

    add store get (#3)
    * add store get
    
    * fix some compiler errs

--- sdstore/sdstore_test.go
+++ sdstore/sdstore_test.go
@@ -1,4 +1,4 @@
-package storeinterface
+package sdstore
 
 import (
 	"bytes"
@@ -62,7 +62,7 @@ func TestFileUpload(t *testing.T) {
 	url := "http://fakeurl"
 	token := "faketoken"
 	testPath := "test/path/1"
-	uploader := &sdUploader{
+	uploader := &sdStore{
 		url,
 		token,
 		&http.Client{Timeout: 10 * time.Second},
@@ -117,7 +117,7 @@ func TestFileUploadRetry(t *testing.T) {
 	url := "http://fakeurl"
 	token := "faketoken"
 	testPath := "test/path/1"
-	uploader := &sdUploader{
+	uploader := &sdStore{
 		url,
 		token,
 		&http.Client{Timeout: 10 * time.Second},
